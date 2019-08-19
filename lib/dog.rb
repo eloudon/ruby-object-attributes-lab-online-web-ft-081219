d6e1b7bbebe94b4e86e3951a232dc231eecd20e5
Eric Loudon <loudon.eric@gmail.com>
2019-08-19 12:38:27 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,12 @@
 class Dog
-def name=(dog_name)
-  @this_dog_name = dog_name
-end
-def name
-  @this_dog_name
-end
+
+  def initialize(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+
 end
